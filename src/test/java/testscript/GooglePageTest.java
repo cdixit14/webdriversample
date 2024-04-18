adcf86cd87736795c376bb08ee0f1dccb6079d3a
chiragdixit74@gmail.com <emailid>
2024-04-18 05:37:35 +0000

    Initial Project

---
+++ src/test/java/testscript/GooglePageTest.java
@@ -0,0 +1,15 @@
+package testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+	}
+
+}
