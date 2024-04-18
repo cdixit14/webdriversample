6af0ca414a08520fe8a7a7439171933422f47fc8
chiragdixit74@gmail.com <emailid>
2024-04-18 07:37:33 +0000

    JAVASearch

--- src/test/java/testscript/GooglePageTest.java
+++ src/test/java/testscript/GooglePageTest.java
@@ -1,15 +1,27 @@
 package testscript;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	//	ChromeOptions options = new ChromeOptions();
+	//	options.setBrowserVersion("115");
+	//	WebDriver driver = new ChromeDriver(options);
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
+		System.out.println("Page title : ...." + driver.getTitle());
+		WebElement srcBox = driver.findElement(By.id("APjFqb"));
+		srcBox.sendKeys("Java Tutorial");
+		srcBox.sendKeys(Keys.ENTER);
+		
 		System.out.println("Page title : ...." + driver.getTitle());
 	}
 
