62d318cecdc2f4877b17c4334d6934f72f5387ee
chiragdixit74@gmail.com <emailid>
2024-04-22 07:25:24 +0000

    LoginPageTest

--- src/test/java/testscript/LoginPageTest.java
+++ src/test/java/testscript/LoginPageTest.java
@@ -12,13 +12,16 @@ public class LoginPageTest {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/login");
-		driver.findElement(By.id("username")).sendKeys("tomsmith");
+	//	driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("tomsmith");
 		driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
 		
-		driver.findElement(By.className("radius")).click();
+	//	driver.findElement(By.className("radius")).click();
 		
-		driver.findElement(By.linkText("Elemental Selenium")).click();
-	//	driver.findElement(By.partialLinkText("Elemental")).click();
+		driver.findElement(By.xpath("//i[@class='fa fa-2x fa-sign-in']")).click();
+		
+	//	driver.findElement(By.linkText("Elemental Selenium")).click();
+		driver.findElement(By.partialLinkText("Elemental")).click();
 		
 	}
 
