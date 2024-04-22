5fc48f067ee5541e54671d048d455d861080f7bf
chiragdixit74@gmail.com <emailid>
2024-04-22 05:01:45 +0000

    Herokuapp_LoginTest

--- src/test/java/testscript/GooglePageTest.java
+++ src/test/java/testscript/GooglePageTest.java
@@ -13,16 +13,34 @@ public class GooglePageTest {
 		// TODO Auto-generated method stub
 	//	ChromeOptions options = new ChromeOptions();
 	//	options.setBrowserVersion("115");
-	//	WebDriver driver = new ChromeDriver(options);
+	//	options.addArguments("--headless");
 		WebDriver driver = new ChromeDriver();
+	//	WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
+	//	driver.get("https://www.google.com/");
+		driver.navigate().to("https://www.google.com/");
 		System.out.println("Page title : ...." + driver.getTitle());
-		WebElement srcBox = driver.findElement(By.id("APjFqb"));
+	//	locators strategy
+	//	WebElement srcBox = driver.findElement(By.id("APjFqb"));
+	//	WebElement srcBox = driver.findElement(By.name("q"));
+	//	WebElement srcBox = driver.findElement(By.className("gLFyf"));
+		WebElement srcBox = driver.findElement(By.tagName("textarea"));
+		
 		srcBox.sendKeys("Java Tutorial");
 		srcBox.sendKeys(Keys.ENTER);
 		
 		System.out.println("Page title : ...." + driver.getTitle());
+		System.out.println("New URL : " + driver.getCurrentUrl());
+		
+		driver.navigate().back();
+		
+		System.out.println("page title after back...." + driver.getCurrentUrl());
+		
+		driver.navigate().forward();
+		
+		
+		
 	}
+	
 
 }
