b455b2d383190d8abeeea72aaa6a0ea0c4b14a24
chiragdixit74@gmail.com <emailid>
2024-04-22 10:41:00 +0000

    HomeworkDay3

--- src/test/java/testscript/WebTableTest.java
+++ src/test/java/testscript/WebTableTest.java
@@ -6,18 +6,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebTableTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		
+		WebDriver driver = new ChromeDriver(options);
+	//	WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demo.seleniumeasy.com/table-sort-search-demo.html");
-		List<WebElement> items = driver.findElements(By.xpath("//td[text()='B. Wagner']//following-sibling::td"));
-	//	List<WebElement> items1 = driver.findElements(By.xpath("//td[contains(text(),'San Francisco')])//preceding-sibling::td"));
-		System.out.println("Number of items..." + items.size());
-		for(WebElement cell:items)
+	//	List<WebElement> items = driver.findElements(By.xpath("//td[text()='B. Wagner']//following-sibling::td"));
+		List<WebElement> items1 = driver.findElements(By.xpath("//td[text()='San Francisco']//preceding-sibling::td[2]"));
+		System.out.println("Number of items..." + items1.size());
+		for(WebElement cell:items1)
 		{
 			System.out.println(cell.getText());
 		}
